65352848eab0d8b02e290174a4fd2a19cc9bf45c
Gabriel Coimbra <gabrielfelipe.coimbra@gmail.com>
2026-04-29 16:02:50 -0300

    refactor: restructure fighter instances into an array and introduce Luta class for fight management in index.php

---
+++ app/POO/Luta.php
@@ -0,0 +1,105 @@
+<?php
+
+require_once "Lutador.php";
+
+class Luta
+{
+    // Atributos
+    private Lutador $desafiante;
+    private Lutador $desafiado;
+    private int $rounds;
+    private bool $aprovada;
+
+    public function getDesafiante()
+    {
+        return $this->desafiante;
+    }
+
+    public function getDesafiado()
+    {
+        return $this->desafiado;
+    }
+
+    public function getRounds()
+    {
+        return $this->rounds;
+    }
+
+    public function getAprovada()
+    {
+        return $this->aprovada;
+    }
+
+    public function setDesafiante(Lutador $desafiante)
+    {
+        $this->desafiante = $desafiante;
+    }
+
+    public function setDesafiado(Lutador $desafiado)
+    {
+        $this->desafiado = $desafiado;
+    }
+
+    public function setRounds(int $rounds)
+    {
+        $this->rounds = $rounds;
+    }
+
+    public function setAprovada(bool $aprovada)
+    {
+        $this->aprovada = $aprovada;
+    }
+
+    public function marcarLuta(Lutador $l1, Lutador $l2)
+    {
+        if ($l1->getCategoria() === $l2->getCategoria() && $l1 != $l2) {
+            $this->setAprovada(true);
+            $this->setDesafiante($l1);
+            $this->setDesafiado($l2);
+        } else {
+            $this->setAprovada(false);
+            echo "<p>Luta não aprovada</p>";
+            return false;
+        }
+
+        return true;
+    }
+
+    public function lutar()
+    {
+        if ($this->getAprovada()) {
+            echo "<p>Luta aprovada</p>";
+            $this->getDesafiante()->apresentar();
+            $this->getDesafiado()->apresentar();
+            $vencedor = rand(0, 2);
+            echo "<p>Vencedor: {$vencedor}</p>";
+            switch ($vencedor) {
+                case 0:
+                    // Empate
+                    echo "<p>Empate!</p>";
+                    $this->getDesafiante()->empatarLuta();
+                    $this->getDesafiado()->empatarLuta();
+                    break;
+                case 1:
+                    // Desafiado vence
+                    echo "<p>{$this->getDesafiado()->getNome()} venceu!</p>";
+                    $this->getDesafiante()->perderLuta();
+                    $this->getDesafiado()->ganharLuta();
+                    $this->getDesafiante()->status();
+                    $this->getDesafiado()->status();
+                    break;
+                case 2:
+                    // Desafiante vence
+                    echo "<p>{$this->getDesafiante()->getNome()} venceu!</p>";
+                    $this->getDesafiante()->ganharLuta();
+                    $this->getDesafiado()->perderLuta();
+                    $this->getDesafiante()->status();
+                    $this->getDesafiado()->status();
+                    break;
+            }
+        } else {
+            echo "Luta não aprovada";
+            return false;
+        }
+    }
+}
--- app/POO/index.php
+++ app/POO/index.php
@@ -13,20 +13,21 @@
 
         <?php
         require_once "Lutador.php";
+        require_once "Luta.php";
 
-        $lutador1 = new Lutador("Pretty Boy", "França", 31, 1.75, 68.9, 11, 2, 1);
-        $lutador2 = new Lutador("Putscript", "Brasil", 29, 1.68, 57.8, 14, 2, 3);
-        $lutador3 = new Lutador("Snapshadow", "EUA", 35, 1.65, 80.9, 12, 2, 1);
-        $lutador4 = new Lutador("Dead Code", "Austrália", 28, 1.93, 81.6, 13, 0, 2);
-        $lutador5 = new Lutador("UFOCobol", "Brasil", 37, 1.70, 119.3, 5, 4, 3);
-        $lutador6 = new Lutador("Nerdaart", "EUA", 30, 1.81, 105.7, 12, 2, 4);
+        $lutadores = array();
 
-        $lutador1->apresentar();
-        $lutador2->apresentar();
-        $lutador3->apresentar();
-        $lutador4->apresentar();
-        $lutador5->apresentar();
-        $lutador6->apresentar();
+        $lutadores[0] = new Lutador("Pretty Boy", "França", 31, 1.75, 68.9, 11, 2, 1);
+        $lutadores[1] = new Lutador("Putscript", "Brasil", 29, 1.68, 57.8, 14, 2, 3);
+        $lutadores[2] = new Lutador("Snapshadow", "EUA", 35, 1.65, 80.9, 12, 2, 1);
+        $lutadores[3] = new Lutador("Dead Code", "Austrália", 28, 1.93, 81.6, 13, 0, 2);
+        $lutadores[4] = new Lutador("UFOCobol", "Brasil", 37, 1.70, 119.3, 5, 4, 3);
+        $lutadores[5] = new Lutador("Nerdaart", "EUA", 30, 1.81, 105.7, 12, 2, 4);
+
+        $lutas = array();
+        $lutas[0] = new Luta();
+        $lutas[0]->marcarLuta($lutadores[0], $lutadores[1]);
+        $lutas[0]->lutar();
 
         ?>
     </pre>
